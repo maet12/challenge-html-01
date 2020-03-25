ac3c92b198454b3bb6f6a58d6c28315115917a70
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-03-25 15:32:58 -0600

    feat(project) created modules, components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponentComponent } from './layout-component/layout-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponentComponent
   ],
   imports: [
     BrowserModule,
